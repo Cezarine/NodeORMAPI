b5db2b647e163a35d81f8028bf60069d981412ac
Cezarine <gcezarines@gmail.com>
2023-02-26 20:15:41 -0300

    CRUD feito -> Vamos pra o modulo 4

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -14,6 +14,48 @@ class PessoaController
             return res.status(500).json(error.message);
         }
     }
+
+    static async getPessoa(req, res){
+        const { id } = req.params;
+        try{
+            const Pessoa = await Database.Pessoas.findOne({where: {id: Number(id)}});
+            return res.status(200).json(Pessoa);
+        }catch(error){
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async postPessoa(req, res){
+        const newPeople = req.body;
+        try{
+            const newPeopleCreate = await Database.Pessoas.create(newPeople);
+            return res.status(200).json(newPeopleCreate);
+        } catch (error){
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async putPessoa(req, res){
+        const { id } = req.params;
+        const newInfo = req.body;
+        try{
+            await Database.Pessoas.update(newInfo, {where: {id: Number(id)}});
+            const PeopleUpdate = await Database.Pessoas.findOne({where: {id: Number(id)}}); 
+            return res.status(200).json(PeopleUpdate);
+        } catch(error){
+            return res.status(500).json(error.message);    
+        }
+    }
+
+    static async deletePessoa(req, res){
+        const { id } = req.params;
+        try{
+            await Database.Pessoas.update({ativo: Number(0)}, {where: {id: Number(id)}});
+            return res.status(200).json({message: `Id ${id} foi deletado com sucesso`});
+        } catch(error){
+            return res.status(500).json(error.message);    
+        }
+    }
 }
 
 module.exports = PessoaController;
--- api/routes/peoplesRoutes.js
+++ api/routes/peoplesRoutes.js
@@ -2,6 +2,10 @@ const { Router } = require('express')
 const PessoaController = require('../controllers/PessoaController.js')
 const router = Router();
 
-router.get('/pessoas', PessoaController.getAllPeople)
+router.get('/pessoas', PessoaController.getAllPeople);
+router.get('/pessoas/:id', PessoaController.getPessoa);
+router.post('/pessoas', PessoaController.postPessoa);
+router.put('/pessoas/:id', PessoaController.putPessoa);
+router.delete('/pessoas/:id', PessoaController.deletePessoa);// Não deleta ele inativa
 
 module.exports = router
